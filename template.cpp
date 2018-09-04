541a355c236dcf41ff89f2173d0ed452db42df44
hiramekun <thescript1210@gmail.com>
2018-09-04 16:23:49 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -10,6 +10,7 @@ using namespace std;
 typedef long long ll;
 #define INF int(1e9)
 #define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
 #define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
 #define EACH(i, mp) for(auto i:mp)
 #define FOR(i, m, n) for(int i = m;i < n;i++)
