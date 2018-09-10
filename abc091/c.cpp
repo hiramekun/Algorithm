29dcf8f47823d146480a76f84ac535b1068c185b
hiramekun <thescript1210@gmail.com>
2018-09-11 02:28:52 +0900

    [WIP] add abc091

---
+++ abc091/c.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+#define INF int(1e9)
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
+#define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
+#define EACH(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+
+
+const int MAX_N = 100;
+int N;
+vector<pair<int, int> > ab, cd;
+
+void solve() {
+
+}
+
+int main() {
+    cin >> N;
+    int t1, t2;
+    REP(i, N) {
+        cin >> t1 >> t2;
+        ab.emplace_back(make_pair(t1, t2));
+    }
+    REP(i, N) {
+        cin >> t1 >> t2;
+        cd.emplace_back(make_pair(t1, t2));
+    }
+    solve();
+    return 0;
+}
