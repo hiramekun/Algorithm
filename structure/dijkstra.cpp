a6fd8c1d724998953d2db980f082bdf0f4379379
hiramekun <thescript1210@gmail.com>
2018-09-13 10:16:30 +0900

    Rewrite dijkstra

--- structure/dijkstra.cpp
+++ structure/dijkstra.cpp
@@ -6,31 +6,30 @@
 #include <unordered_map>
 #include <cmath>
 
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+
 const int INF = 1000000000;
 using namespace std;
 const int MAX_V = 10000;
-int cost[MAX_V][MAX_V];
-int d[MAX_V];
-bool used[MAX_V];
-int V;
+int cost[MAX_V][MAX_V]; // cost[i][j]: i->jのコスト. 辺がない場合はINFにする.
+int d[MAX_V];           // d[i]: iへの最短距離.
+bool used[MAX_V];       // used[i]: iがすでに使われたかどうか.
+int V;                  // 頂点の数.
 
 // 始点sから各頂点への最短距離を求める．
 void dijkstra(int s) {
     fill(d, d + V, INF);
     fill(used, used + V, false);
-    d[s] = 0;
+    d[s] = 0; // 始点は0
 
     while (true) {
         int v = -1;
-        for (int i = 0; i < V; ++i) {
-            if (!used[i] && (v == -1 || d[i] < d[v])) v = i;
-        }
+        // 使われてないかつ短い距離のものがあればそれを採用.
+        REP(i, V) if (!used[i] && (v == -1 || d[i] < d[v])) v = i;
         if (v == -1) break;
         used[v] = true;
-
-        for (int j = 0; j < V; ++j) {
-            d[j] = min(d[j], d[v] + cost[v][j]);
-        }
+        // iへの距離は，d[i]と，最短距離のd[v]とv->iの合計の小さいほう．
+        REP(i, V) d[i] = min(d[i], d[v] + cost[v][i]);
     }
 }
 
