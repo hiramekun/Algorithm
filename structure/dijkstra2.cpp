ae75530429a7c885c326813ea2076df66dcf988a
hiramekun <thescript1210@gmail.com>
2018-09-13 10:38:18 +0900

    Rewrite dijkstra2

--- structure/dijkstra2.cpp
+++ structure/dijkstra2.cpp
@@ -7,37 +7,43 @@
 #include <cmath>
 #include <queue>
 
-const int INF = 1000000000;
 using namespace std;
+typedef long long ll;
+#define INF int(1e9)
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
+#define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
+#define EACH(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+
 const int MAX_V = 10000;
 
 struct edge {
     int to, cost;
 };
-
-typedef pair<int, int> P;
+typedef pair<int, int> P; // first: 最短距離, second: 頂点の番号
 
 int V;
 vector<edge> G[MAX_V];
 int d[MAX_V];
 
-// 始点sから各頂点への最短距離を求める．
 void dijkstra(int s) {
-    // greater<P>を指定することでfirstが小さい順に取り出せる様にする
+    // greater<P>を指定することでfirstが小さい順に取り出せるようにする
     priority_queue<P, vector<P>, greater<> > que;
     fill(d, d + V, INF);
     d[s] = 0;
     que.push(P(0, s));
 
     while (!que.empty()) {
-        P p = que.top();
+        P p = que.top(); // queから最短距離が最小のものを取り出す
         que.pop();
-        int v = p.second;
-        if (d[v] < p.first) continue;
-        for (int i = 0; i < G[v].size(); i++) {
+        int v = p.second; // 最短距離が最小のものの頂点
+        if (d[v] < p.first) continue; // すでに最短距離が小さければ
+        REP(i, G[v].size()) {
             edge e = G[v][i];
             if (d[e.to] > d[v] + e.cost) {
                 d[e.to] = d[v] + e.cost;
+                // 頂点vから張っている頂点への最短距離を求め，それを次にqueに入れる．
                 que.push(P(d[e.to], e.to));
             }
         }
@@ -47,4 +53,3 @@ void dijkstra(int s) {
 int main() {
     return 0;
 }
-
