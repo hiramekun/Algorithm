cc78f1f024563ae77ae97b4b564306adfc5863c8
hiramekun <thescript1210@gmail.com>
2018-10-14 03:26:56 +0900

    Add agc028

---
+++ agc028/a.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define INF int(1e9)
+#define REP(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
+#define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
+#define EACH(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+ll N, M;
+string S, T;
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll m, ll n) {
+    if ((0 == m) || (0 == n)) return 0;
+    return ((m / gcd(m, n)) * n);
+}
+
+void solve() {
+    ll L = lcm(N, M);
+    REP(i, M / (L / N)) {
+        if (S[L / M * i] != T[L / N * i]) {
+            cout << "-1" << endl;
+            return;
+        }
+    }
+    cout << L << endl;
+}
+
+int main() {
+    cin >> N >> M;
+    cin >> S >> T;
+    solve();
+    return 0;
+}
