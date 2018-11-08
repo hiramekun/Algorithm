b37aa13a5ecc7a79d6d55d12ca85b2d2306b14df
hiramekun <thescript1210@gmail.com>
2018-11-09 00:24:58 +0900

    Fix util

--- util/each_digit.cpp
+++ util/each_digit.cpp
@@ -1,17 +1,23 @@
 #include <cstdio>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
+typedef long long ll;
 const int MAX_S = 10;
 int s_len, s_dig[MAX_S];
 
+
 // 1234 -> 4 3 2 1
-int each_digit(int S) {
+vector<int> each_digit(ll S) {
+    int n_len = 0, n_dig[MAX_S];
     while (S > 0) {
-        s_dig[s_len++] = S % 10;
+        n_dig[n_len++] = S % 10;
         S /= 10;
     }
+    vector<int> ret(n_dig, n_dig + n_len);
+    return ret;
 }
 
 int main() {
