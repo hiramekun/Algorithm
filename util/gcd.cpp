2c34379eb2a022e88ce14f490a1c92acb16c4753
hiramekun <thescript1210@gmail.com>
2018-11-23 16:37:40 +0900

    Change to ll

--- util/gcd.cpp
+++ util/gcd.cpp
@@ -1,14 +1,15 @@
 #include <cstdio>
 #include <iostream>
 
+typedef long long ll;
 using namespace std;
 
-int gcd(int a, int b) {
+ll gcd(ll a, ll b) {
     if (b == 0) return a;
     return gcd(b, a % b);
 }
 
-int lcm(int m, int n) {
+ll lcm(ll m, ll n) {
     if ((0 == m) || (0 == n)) return 0;
     return ((m / gcd(m, n)) * n);
 }
