73e71990ad0d47a123f721547381187018bfcc46
hiramekun <thescript1210@gmail.com>
2018-11-23 15:04:48 +0900

    Refactor

--- abc051/c.cpp
+++ abc051/c.cpp
@@ -22,61 +22,15 @@ int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 int sx, sy, tx, ty;
 
 void solve() {
-    if (tx > sx) {
-        REP(i, tx - sx) putchar('R');
-        if (ty > sy) {
-            REP(i, ty - sy) putchar('U');
-            REP(i, tx - sx) putchar('L');
-            REP(i, ty - sy)putchar('D');
-            putchar('L');
-            REP(i, ty - sy + 1) putchar('U');
-            REP(i, tx - sx + 1) putchar('R');
-            putchar('D');
-            putchar('R');
-            REP(i, ty - sy + 1) putchar('D');
-            REP(i, tx - sx + 1) putchar('L');
-            putchar('U');
-        } else {
-            REP(i, sy - ty) putchar('D');
-            REP(i, tx - sx) putchar('L');
-            REP(i, sy - ty)putchar('U');
-            putchar('L');
-            REP(i, sy - ty + 1) putchar('D');
-            REP(i, tx - sx + 1) putchar('R');
-            putchar('U');
-            putchar('R');
-            REP(i, sy - ty + 1) putchar('U');
-            REP(i, tx - sx + 1) putchar('L');
-            putchar('D');
-        }
-    } else {
-        REP(i, sx - tx) putchar('L');
-        if (ty > sy) {
-            REP(i, ty - sy) putchar('U');
-            REP(i, sx - tx) putchar('R');
-            REP(i, ty - sy)putchar('D');
-            putchar('R');
-            REP(i, ty - sy + 1) putchar('U');
-            REP(i, sx - tx + 1) putchar('L');
-            putchar('D');
-            putchar('L');
-            REP(i, ty - sy + 1) putchar('D');
-            REP(i, sx - tx + 1) putchar('R');
-            putchar('U');
-        } else {
-            REP(i, sy - ty) putchar('D');
-            REP(i, sx - tx) putchar('R');
-            REP(i, sy - ty)putchar('U');
-            putchar('R');
-            REP(i, sy - ty + 1) putchar('D');
-            REP(i, sx - tx + 1) putchar('L');
-            putchar('U');
-            putchar('L');
-            REP(i, sy - ty + 1) putchar('U');
-            REP(i, sx - tx + 1) putchar('R');
-            putchar('D');
-        }
-    }
+    int dx = tx - sx;
+    int dy = ty - sy;
+    cout << string(dx, 'R') << string(dy, 'U') << string(dx, 'L') << string(dy, 'D');
+    putchar('L');
+    cout << string(dy + 1, 'U') << string(dx + 1, 'R');
+    putchar('D');
+    putchar('R');
+    cout << string(dy + 1, 'D') << string(dx + 1, 'L');
+    putchar('U');
 }
 
 int main() {
