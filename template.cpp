2073d439853c98d32702e0200c772af6463fc182
hiramekun <thescript1210@gmail.com>
2018-12-01 20:24:10 +0900

    Change to lower case

--- template.cpp
+++ template.cpp
@@ -12,13 +12,13 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> P;
-#define i_INF int(1e9)
-#define ll_INF ll(1e9)*ll(1e9)
-#define REP(i, n) for(ll i = 0; i < (ll)(n); i++)
-#define REPR(i, n) for(ll i = n - 1; i >= 0; i--)
-#define REPONE(i, n) for(ll i = 1; i <= (ll)(n); i++)
-#define EACH(i, mp) for(auto i:mp)
-#define FOR(i, m, n) for(ll i = m;i < n;i++)
+#define i_inf int(1e9)
+#define ll_inf ll(1e9)*ll(1e9)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define for(i, m, n) for(ll i = m;i < n;i++)
 int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
 
