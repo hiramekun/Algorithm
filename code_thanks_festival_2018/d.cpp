f60d321224dee7b75f2394b804d9d437eee57d9d
hiramekun <thescript1210@gmail.com>
2018-12-02 01:50:46 +0900

    Add d

---
+++ code_thanks_festival_2018/d.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define i_inf int(1e9)
+#define ll_inf ll(1e9)*ll(1e9)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+
+string s;
+
+void solve() {
+    char current = s[0];
+    int idx = 1, ans = 1;
+    while (idx < s.size()) {
+        if (s[idx] <= current) {
+            ans++;
+            current = s[idx];
+        }
+        idx++;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin >> s;
+    solve();
+    return 0;
+}
