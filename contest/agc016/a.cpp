ddf688803ca41c5a5724bb829a91664551089c14
hiramekun <thescript1210@gmail.com>
2018-12-06 21:27:53 +0900

    Rewrite with all search

--- contest/agc016/a.cpp
+++ contest/agc016/a.cpp
@@ -19,25 +19,35 @@ typedef pair<int, int> P;
 #define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define reprone(i, n) for(ll i = n; i >= 1; i--)
 #define each(i, mp) for(auto i:mp)
 #define FOR(i, m, n) for(ll i = m;i < n;i++)
 int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
 string s;
-unordered_map<char, vector<int> > mp;
+
+bool all_same(string s, char target) {
+    rep(i, s.length()) if (s[i] != target) return false;
+    return true;
+}
 
 void solve() {
     int ans = i_inf;
-    rep(i, s.length()) mp[s[i]].emplace_back(i);
-    each(m, mp) {
-        int pre = -1, d_max = 0, to_last = i_inf;
-        each(i, m.second) {
-            int d = pre == -1 ? i : i - pre - 1;
-            pre = i;
-            d_max = max(d_max, d);
-            to_last = min(to_last, int(s.length() - 1 - i));
+    for (int i = 'a'; i <= 'z'; ++i) {
+        string s1(s);
+        int temp = 0;
+        while (s1.length() != 0 && !all_same(s1, char(i))) {
+            reprone(j, s1.length() - 1) {
+                if (s1[j] == char(i) && s1[j - 1] != char(i)) {
+                    s1[j - 1] = char(i);
+                    j--;
+                    if (j == 0) break;
+                }
+            }
+            temp++;
+            s1.erase(s1.end() - 1);
         }
-        ans = min(max(d_max, to_last), ans);
+        if (all_same(s1, char(i))) ans = min(ans, temp);
     }
     cout << ans << endl;
 }
