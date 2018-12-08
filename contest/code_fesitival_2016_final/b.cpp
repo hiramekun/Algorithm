3314c0e6c27f420d4357d30455e4d0a01aaca2e1
hiramekun <thescript1210@gmail.com>
2018-12-08 16:46:41 +0900

    Add code festival 2016 final

---
+++ contest/code_fesitival_2016_final/b.cpp
@@ -0,0 +1,47 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define i_inf int(1e9)
+#define i_half_inf int(1e5)
+#define ll_inf ll(1e9)*ll(1e9)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+int n;
+
+void solve() {
+    vector<int> ans;
+    int sum = 0;
+    repone(i, n) {
+        if (sum < n) {
+            sum += i;
+            ans.emplace_back(i);
+        } else break;
+    }
+    each(i, ans) {
+        if (i == sum - n) continue;
+        else cout << i << endl;
+    }
+}
+
+int main() {
+    cin >> n;
+    solve();
+    return 0;
+}
