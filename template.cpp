e147491357561e778a27e08636ec16f2db18e92e
hiramekun <thescript1210@gmail.com>
2018-12-23 02:22:39 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -13,16 +13,16 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> P;
-#define inf int(1e9)
-#define half_inf int(1e5)
-#define ll_inf ll(1e9)*ll(1e9)
 #define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
 #define each(i, mp) for(auto i:mp)
 #define FOR(i, m, n) for(ll i = m;i < n;i++)
 const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-const int mod = 1000000007;
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
 
 
 void solve() {
