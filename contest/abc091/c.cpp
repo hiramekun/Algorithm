5240d6f7e90b26037f73eb644f3df8d9bc040165
hiramekun <thescript1210@gmail.com>
2018-12-23 21:22:02 +0900

    Add abc091

--- contest/abc091/c.cpp
+++ contest/abc091/c.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <cstdio>
 #include <algorithm>
 #include <iostream>
@@ -5,35 +6,59 @@
 #include <map>
 #include <unordered_map>
 #include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
 
 using namespace std;
 typedef long long ll;
-#define INF int(1e9)
-#define REP(i, n) for(int i = 0; i < (int)(n); i++)
-#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
-#define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
-#define EACH(i, mp) for(auto i:mp)
-#define FOR(i, m, n) for(int i = m;i < n;i++)
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
 
-
-const int MAX_N = 100;
-int N;
-vector<pair<int, int> > ab, cd;
+int n;
+vector<P> ab, cd;
 
 void solve() {
-
+    sort(ab.begin(), ab.end(), [](P a, P b) { return a.second > b.second; });
+    sort(cd.begin(), cd.end());
+    int ans = 0;
+    bool used[n];
+    fill(used, used + n, false);
+    rep(i, n) {
+        P _xy = cd[i];
+        rep(j, n) {
+            P xy = ab[j];
+            if (!used[j] && xy.first < _xy.first && xy.second < _xy.second) {
+                used[j] = true;
+                ans++;
+                break;
+            }
+        }
+    }
+    cout << ans << endl;
 }
 
 int main() {
-    cin >> N;
-    int t1, t2;
-    REP(i, N) {
-        cin >> t1 >> t2;
-        ab.emplace_back(make_pair(t1, t2));
+    cin >> n;
+    ab.resize(n);
+    cd.resize(n);
+    int x, y;
+    rep(i, n) {
+        cin >> x >> y;
+        ab[i] = make_pair(x, y);
     }
-    REP(i, N) {
-        cin >> t1 >> t2;
-        cd.emplace_back(make_pair(t1, t2));
+    rep(i, n) {
+        cin >> x >> y;
+        cd[i] = make_pair(x, y);
     }
     solve();
     return 0;
