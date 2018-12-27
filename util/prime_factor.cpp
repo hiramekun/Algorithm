77454df0374ca2cbfe8dffb13a5f734d75175d40
hiramekun <thescript1210@gmail.com>
2018-12-27 10:51:06 +0900

    Chamge template

--- util/prime_factor.cpp
+++ util/prime_factor.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <cstdio>
 #include <algorithm>
 #include <iostream>
@@ -7,18 +8,21 @@
 #include <cmath>
 #include <queue>
 #include <set>
+#include <stack>
 
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> P;
-#define INF int(1e9)
-#define REP(i, n) for(int i = 0; i < (int)(n); i++)
-#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
-#define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
-#define EACH(i, mp) for(auto i:mp)
-#define FOR(i, m, n) for(int i = m;i < n;i++)
-int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
 map<ll, ll> mp;
 
 // 素因数分解
