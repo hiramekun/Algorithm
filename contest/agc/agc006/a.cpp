b039ecca09a0f4a9d9aba9576b66ef847982a863
hiramekun <thescript1210@gmail.com>
2018-12-29 19:21:42 +0900

    Add agc006

---
+++ contest/agc/agc006/a.cpp
@@ -0,0 +1,44 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+int n;
+string s, t;
+
+void solve() {
+    int idx_t = 0;
+    rep(i, n) {
+        while (i < n && idx_t < n && s[i] == t[idx_t]) {
+            i++, idx_t++;
+        }
+    }
+    cout << n + n - idx_t;
+}
+
+int main() {
+    cin >> n >> s >> t;
+    solve();
+    return 0;
+}
