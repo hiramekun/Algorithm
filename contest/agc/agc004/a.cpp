cf61ee052eeae04a21eb835534e3b51a9a3c691d
hiramekun <thescript1210@gmail.com>
2018-12-29 19:36:14 +0900

    Add agc004

---
+++ contest/agc/agc004/a.cpp
@@ -0,0 +1,41 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+ll a, b, c;
+
+void solve() {
+    if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) cout << 0 << endl;
+    else
+        cout << min({-a * b * (c / 2) + a * b * (c - c / 2),
+                     -(a / 2) * b * c + (a - a / 2) * b * c,
+                     -a * (b / 2) * c + a * (b - b / 2) * c}) << endl;
+}
+
+int main() {
+    cin >> a >> b >> c;
+    solve();
+    return 0;
+}
