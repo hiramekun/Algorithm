72bbe9fbb5773217b8b795a5bb6dd801d930aa15
hiramekun <thescript1210@gmail.com>
2018-12-30 00:23:51 +0900

    Add agc030

---
+++ contest/agc/agc030/b.cpp
@@ -0,0 +1,45 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+ll l, n;
+vector<ll> x, dis;
+
+void solve() {
+    ll ans = 0;
+}
+
+int main() {
+    cin >> l >> n;
+    x.resize(n);
+    dis.resize(n);
+    rep(i, n) {
+        cin >> x[i];
+        if (x[i] > l / 2) dis[i] = -(l - x[i]);
+        else dis[i] = x[i];
+    }
+    solve();
+    return 0;
+}
