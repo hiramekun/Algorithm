ed45018a8f70408bec46ae68734e38d001e4ad9a
hiramekun <thescript1210@gmail.com>
2019-01-28 07:18:31 +0900

    Add c

--- contest/nikkei2019/c.cpp
+++ contest/nikkei2019/c.cpp
@@ -25,52 +25,26 @@ const ll half_inf = ll(1e5);
 const ll ll_inf = ll(1e9) * ll(1e9);
 
 ll n;
-struct dish {
-    ll a, b;
-    ll idx;
-};
-vector<dish> ds;
+vector<pair<ll, ll>> ab;
 
 void solve() {
-    sort(ds.begin(), ds.end(), [](dish d1, dish d2) {
-        if (d1.a == d2.a) return d1.b > d2.b;
-        else return d1.a > d2.a;
-    });
-    vector<dish> ds_c(ds);
-    sort(ds_c.begin(), ds_c.end(), [](dish d1, dish d2) {
-        if (d1.b == d2.b) return d1.a > d2.a;
-        else return d1.b > d2.b;
-    });
+    // sort from bigger.
+    sort(ab.begin(), ab.end(),
+         [](pair<ll, ll> a, pair<ll, ll> b) { return a.first + a.second > b.first + b.second; });
     ll ans = 0;
-    unordered_map<ll, ll> used;
-    ll idx_t = 0, idx_a = 0;
-
-    while (idx_t < n || idx_a < n) {
-        while (idx_t < n - 1 && used[ds[idx_t].idx] == 1) idx_t++;
-        if (used[ds[idx_t].idx] == 0) {
-            ans += ds[idx_t].a;
-            used[ds[idx_t].idx]++;
-        } else break;
-        idx_t++;
-
-        while (idx_a < n - 1 && used[ds_c[idx_a].idx] == 1) idx_a++;
-        if (used[ds_c[idx_a].idx] == 0) {
-            ans -= ds_c[idx_a].b;
-            used[ds_c[idx_a].idx]++;
-        }
-        idx_a++;
+    rep(i, n) {
+        if (i % 2 == 0) ans += ab[i].first;
+        else ans -= ab[i].second;
     }
     cout << ans << endl;
 }
 
 int main() {
     cin >> n;
-    ds.resize(n);
     rep(i, n) {
         ll a, b;
         cin >> a >> b;
-        dish d = {a, b, i};
-        ds[i] = d;
+        ab.emplace_back(make_pair(a, b));
     }
     solve();
     return 0;
