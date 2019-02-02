71a53ce51268c8b693b2dc721f218ebf74563f83
hiramekun <thescript1210@gmail.com>
2019-02-02 23:13:04 +0900

    Add agc_a

--- contest/agc/agc017/a.cpp
+++ contest/agc/agc017/a.cpp
@@ -28,7 +28,32 @@ ll n, p;
 vector<ll> a;
 
 
+vector<vector<ll>> comb(int n, int r) {
+    vector<vector<ll>> v(n + 1, vector<ll>(n + 1, 0));
+    for (int i = 0; i < v.size(); i++) {
+        v[i][0] = 1;
+        v[i][i] = 1;
+    }
+    for (int j = 1; j < v.size(); j++) {
+        for (int k = 1; k < j; k++) {
+            v[j][k] = (v[j - 1][k - 1] + v[j - 1][k]);
+        }
+    }
+    return v;
+}
+
 void solve() {
+    vector<ll> e, o;
+    rep(i, n) if (a[i] % 2 == 0)e.emplace_back(a[i]); else o.emplace_back(a[i]);
+    ll o_temp = 0;
+    repone(i, o.size()) {
+        if (i % 2 == 1) {
+            if (i == o.size())o_temp++;
+            else o_temp += comb(o.size(), i)[o.size()][i];
+        }
+    }
+    if (p == 0) cout << ll(pow(ll(2), n)) - ll(pow(ll(2), ll(e.size()))) * o_temp << endl;
+    else cout << ll(pow(ll(2), ll(e.size()))) * o_temp << endl;
 }
 
 int main() {
