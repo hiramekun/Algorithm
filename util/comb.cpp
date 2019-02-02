485dce292f861114b8cc14600ac1ac1eeee68cb7
hiramekun <thescript1210@gmail.com>
2019-02-02 23:13:23 +0900

    Add comb

--- util/comb.cpp
+++ util/comb.cpp
@@ -19,6 +19,20 @@ typedef pair<int, int> P;
 #define FOR(i, m, n) for(int i = m;i < n;i++)
 int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
+// nCr: v[n][r]
+vector<vector<ll>> comb(ll n, ll r) {
+    vector<vector<ll>> v(n + 1, vector<ll>(n + 1, 0));
+    for (int i = 0; i < v.size(); i++) {
+        v[i][0] = 1;
+        v[i][i] = 1;
+    }
+    for (int j = 1; j < v.size(); j++) {
+        for (int k = 1; k < j; k++) {
+            v[j][k] = (v[j - 1][k - 1] + v[j - 1][k]);
+        }
+    }
+    return v;
+}
 
 void comb(int n, int k) {
     int arr[] = {1, 2, 3, 4}; // array must be sorted
