7340b03fd00188d792e8c5c343ad1db235f440fe
hiramekun <thescript1210@gmail.com>
2019-02-11 09:48:36 +0900

    Add get_path

--- util/dijkstra_que.cpp
+++ util/dijkstra_que.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 typedef long long ll;
-#define INF int(1e9)
+const ll inf = ll(1e9);
 #define REP(i, n) for(int i = 0; i < (int)(n); i++)
 #define REPR(i, n) for(int i = n - 1; i >= 0; i--)
 #define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
@@ -26,11 +26,13 @@ typedef pair<int, int> P; // first: 最短距離, second: 頂点の番号
 int V;
 vector<edge> G[MAX_V];
 int d[MAX_V];
+int pre[MAX_V]; // 最短路の直前の頂点
 
 void dijkstra(int s) {
     // greater<P>を指定することでfirstが小さい順に取り出せるようにする
     priority_queue<P, vector<P>, greater<> > que;
-    fill(d, d + V, INF);
+    fill(d, d + V, inf);
+    fill(pre, pre + V, -1);
     d[s] = 0;
     que.push(P(0, s));
 
@@ -43,11 +45,20 @@ void dijkstra(int s) {
             edge e = G[v][i];
             if (d[e.to] > d[v] + e.cost) {
                 d[e.to] = d[v] + e.cost;
+                pre[e.to] = v;
                 // 頂点vから張っている頂点への最短距離を求め，それを次にqueに入れる．
                 que.push(P(d[e.to], e.to));
             }
         }
     }
+}
+
+// 頂点tへの最短路
+vector<int> get_path(int t) {
+    vector<int> path;
+    for (; t != -1; t = pre[t]) path.emplace_back(t); // tがsになるまでpre[t]をたどっていく
+    reverse(path.begin(), path.end());
+    return path;
 }
 
 int main() {
