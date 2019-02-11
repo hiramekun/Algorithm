8ba672d2a8d061e829f04fe0eaeb00bac50291c1
hiramekun <thescript1210@gmail.com>
2019-02-11 09:41:04 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -17,7 +17,6 @@ typedef pair<int, int> P;
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
 #define each(i, mp) for(auto i:mp)
-#define FOR(i, m, n) for(ll i = m;i < n;i++)
 const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 const ll mod = 1000000007;
 const ll inf = ll(1e9);
