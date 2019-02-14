2faa61f3d420461a68ac8a13519538d42f5c1317
hiramekun <thescript1210@gmail.com>
2019-02-14 19:47:34 +0900

    Add virtual contest 20190213

---
+++ practice/virtual_contest/20190213/Bridge.cpp
@@ -0,0 +1,67 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+int n, m;
+int a[50], b[50];
+bool seen[50];
+
+int G[50][50];
+
+void dfs(int now) {
+    seen[now] = true;
+    rep(i, n) {
+        if (!G[now][i])continue;
+        if (seen[i]) continue;
+        dfs(i);
+    }
+}
+
+void solve() {
+    int ans = 0;
+    rep(i, m) {
+        G[a[i]][b[i]] = G[b[i]][a[i]] = false;
+        fill(seen, seen + n, false);
+        dfs(0);
+        rep(j, n) if (!seen[j]) {
+                ans++;
+                break;
+            }
+        G[a[i]][b[i]] = G[b[i]][a[i]] = true;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin >> n >> m;
+    rep(i, m) {
+        int ta, tb;
+        cin >> ta >> tb;
+        ta--, tb--;
+        G[ta][tb] = G[tb][ta] = true;
+        a[i] = ta, b[i] = tb;
+    }
+    solve();
+    return 0;
+}
