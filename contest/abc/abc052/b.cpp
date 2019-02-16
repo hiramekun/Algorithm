d410999d8802c7f6c9913a8484d25eea479d505e
hiramekun <thescript1210@gmail.com>
2019-02-16 20:40:23 +0900

    Add ab052 b

---
+++ contest/abc/abc052/b.cpp
@@ -0,0 +1,39 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    int x = 0;
+    int ans = 0;
+    rep(i, n) {
+        if (s[i] == 'I')x++; else x--;
+        ans = max(ans, x);
+    }
+    cout << ans << endl;
+    return 0;
+}
