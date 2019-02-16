296556c845d94d8d40f0465ace398a8a759de4d6
hiramekun <thescript1210@gmail.com>
2019-02-16 20:07:49 +0900

    Add abc089 b

---
+++ contest/abc/abc089/b.cpp
@@ -0,0 +1,39 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+
+int main() {
+    int n;
+    set<char> s;
+    cin >> n;
+    rep(i, n) {
+        char temp;
+        cin >> temp;
+        s.insert(temp);
+    }
+    if (s.size() == 4) cout << "Four" << endl;
+    else cout << "Three" << endl;
+    return 0;
+}
