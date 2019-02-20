a080131a439b1d6ed81e5228deabf03f3507900c
hiramekun <thescript1210@gmail.com>
2019-02-21 01:24:57 +0900

    Add virtual contest 20190220

---
+++ practice/virtual_contest/20190220/Chocolate.cpp
@@ -0,0 +1,53 @@
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+unordered_map<ll, ll> mp;
+
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+        mp = unordered_map<ll, ll>();
+#pragma clang diagnostic pop
+#endif
+
+        int n, d, x;
+        cin >> n >> d >> x;
+        vector<int> a(n);
+        rep(i, n)cin >> a[i];
+        rep(i, n) {
+            x += ceil(double(d) / a[i]);
+        }
+        cout << x << endl;
+
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
