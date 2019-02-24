a3c83fc7816808f2ba0c656dbc447fafb7ca0a7a
hiramekun <thescript1210@gmail.com>
2019-02-24 19:12:02 +0900

    Add virtual contest 201902223 d

--- practice/virtual_contest/20190223/GoodGrid.cpp
+++ practice/virtual_contest/20190223/GoodGrid.cpp
@@ -13,7 +13,6 @@
 
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> P;
 #define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
@@ -23,8 +22,10 @@ const ll mod = 1000000007;
 const ll inf = ll(1e9);
 const ll half_inf = ll(1e5);
 const ll ll_inf = ll(1e9) * ll(1e9);
+typedef unordered_map<ll, ll> ump;
+typedef pair<ll, ll> P;
 
-unordered_map<ll, ll> mp;
+ump mp;
 
 void solve() {
 }
@@ -34,11 +35,38 @@ int main() {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true) {
-        mp = unordered_map<ll, ll>();
+        mp = ump();
 #pragma clang diagnostic pop
 #endif
+        ll n, C;
+        cin >> n >> C;
+        ll D[C][C];
+        rep(i, C) rep(j, C) cin >> D[i][j];
+        ll c[n][n];
+        ll c_count[3][C];
+        rep(i, 3) rep(j, C) c_count[i][j] = 0;
+        rep(i, n) {
+            rep(j, n) {
+                cin >> c[i][j], c[i][j]--;
+                c_count[(i + 1 + j + 1) % 3][c[i][j]]++;
+            }
+        }
 
-        solve();
+        ll ans = ll_inf;
+        rep(k, C) {
+            rep(l, C) {
+                if (k == l) continue;
+                rep(m, C) {
+                    if (l == m || m == k) continue;
+                    ll temp = 0;
+                    rep(i, C) temp += D[i][k] * c_count[0][i];
+                    rep(i, C) temp += D[i][l] * c_count[1][i];
+                    rep(i, C) temp += D[i][m] * c_count[2][i];
+                    ans = min(temp, ans);
+                }
+            }
+        }
+        cout << ans << endl;
 
 #ifdef MY_DEBUG
     }
