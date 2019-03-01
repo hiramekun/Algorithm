c73f904fc62f4c61a7590a5e87f1fe4a1e550154
hiramekun <thescript1210@gmail.com>
2019-03-01 21:36:05 +0900

    Add virtual contest 20190301 d

--- practice/virtual_contest/20190301/SunukeColoring.cpp
+++ practice/virtual_contest/20190301/SunukeColoring.cpp
@@ -22,12 +22,41 @@ const ll mod = 1000000007;
 const ll inf = ll(1e9);
 const ll half_inf = ll(1e5);
 const ll ll_inf = ll(1e9) * ll(1e9);
-typedef unordered_map<ll, ll> ump;
+typedef map<pair<ll, ll>, ll> ump;
 typedef pair<ll, ll> P;
 
 ump mp;
+ll h, w, n;
 
 void solve() {
+    map<int, ll> ans;
+    set<pair<ll, ll>> see;
+    each(m, mp) {
+        ll x = m.first.first;
+        ll y = m.first.second;
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                int nx = x + i, ny = y + j;
+                if (nx >= 1 && ny >= 1 && nx <= h - 2 && ny <= w - 2) {
+                    see.insert(make_pair(nx, ny));
+                }
+            }
+        }
+    }
+    each(s, see) {
+        ll x = s.first;
+        ll y = s.second;
+        ll tmp = 0;
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                int nx = x + i, ny = y + j;
+                tmp += mp[make_pair(nx, ny)];
+            }
+        }
+        ans[tmp]++;
+    }
+    ans[0] = (h - 2) * (w - 2) - see.size();
+    rep(i, 10) cout << ans[i] << endl;
 }
 
 int main() {
@@ -38,6 +67,13 @@ int main() {
         mp = ump();
 #pragma clang diagnostic pop
 #endif
+        cin >> h >> w >> n;
+        rep(i, n) {
+            ll a, b;
+            cin >> a >> b;
+            a--, b--;
+            mp[make_pair(a, b)]++;
+        }
 
         solve();
 
