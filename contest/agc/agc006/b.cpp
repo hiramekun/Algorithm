3bf986aa3eae51c121eea049774893f7c89f7354
hiramekun <thescript1210@gmail.com>
2019-03-08 01:25:47 +0900

    Add agc060 b

---
+++ contest/agc/agc006/b.cpp
@@ -0,0 +1,65 @@
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <numeric>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+typedef unordered_map<ll, ll> ump;
+typedef pair<ll, ll> P;
+
+ump mp;
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+        mp = ump();
+#pragma clang diagnostic pop
+#endif
+        int n, x;
+        cin >> n >> x;
+        if (x == 1 || x == 2 * n - 1) cout << "No" << endl;
+        else {
+            cout << "Yes" << endl;
+            int ans[2 * n - 1];
+            ans[n - 2] = x - 1;
+            ans[n - 1] = x;
+            ans[n] = x + 1;
+            int temp = 1;
+            rep(i, 2 * n - 1) {
+                if (temp == x - 1) temp += 3;
+                if (i != n - 2 && i != n - 1 && i != n) {
+                    cout << temp << endl;
+                    temp++;
+                } else {
+                    cout << ans[i] << endl;
+                }
+            }
+        }
+
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
