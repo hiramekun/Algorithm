32d9c37d4e1bd6a7a54655632ff114fdd15acda7
hiramekun <thescript1210@gmail.com>
2019-04-01 00:26:36 +0900

    Add AOJ TheNumberOfWindows

---
+++ practice/AOJ/TheNumberOfWindows.cpp
@@ -0,0 +1,72 @@
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <numeric>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+typedef unordered_map<ll, ll> ump;
+typedef pair<ll, ll> P;
+
+ump mp;
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+        mp = ump();
+#pragma clang diagnostic pop
+#endif
+        ll n, q;
+        cin >> n >> q;
+        vector<ll> a(n), x(q);
+        rep(i, n) cin >> a[i];
+        rep(i, q) cin >> x[i];
+
+        // solve
+        rep(i, q) {
+            ll right = 0;
+            ll sum = 0;
+            ll res = 0;
+            for (ll left = 0; left < n; ++left) {
+                while (right < n && sum + a[right] <= x[i]) {
+                    /* 実際に right を 1 進める */
+                    sum += a[right];
+                    ++right;
+                }
+
+                /* break した状態で right は条件を満たす最大なので、何かする */
+                res += (right - left);
+
+                /* left をインクリメントする準備 */
+                if (right == left) ++right;
+                else sum -= a[left];
+            }
+            cout << res << endl;
+        }
+
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
