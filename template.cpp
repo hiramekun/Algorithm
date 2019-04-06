039aeccfcdfb9a067d8339ee5ad67f662e0a2f23
hiramekun <thescript1210@gmail.com>
2019-04-06 18:22:56 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -1,10 +1,10 @@
 #include <cstring>
-#include <iomanip>
 #include <sstream>
 #include <cstdio>
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <map>
 #include <unordered_map>
 #include <cmath>
@@ -19,25 +19,26 @@ typedef long long ll;
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
 #define each(i, mp) for(auto i:mp)
+#define eb emplace_back
+#define F first
+#define S second
 const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 const ll mod = 1000000007;
 const ll inf = ll(1e9);
 const ll half_inf = ll(1e5);
 const ll ll_inf = ll(1e9) * ll(1e9);
-typedef unordered_map<ll, ll> ump;
+typedef unordered_map<ll, ll> mpll;
+typedef unordered_map<char, ll> mpcl;
 typedef pair<ll, ll> P;
-
-ump mp;
+typedef vector<ll> vll;
 
 int main() {
 #ifdef MY_DEBUG
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true) {
-        mp = ump();
 #pragma clang diagnostic pop
 #endif
-
 
 #ifdef MY_DEBUG
     }
