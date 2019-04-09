2f7899bd0b975bdddca78c45f377d3db48a9dc1d
hiramekun <thescript1210@gmail.com>
2019-04-10 01:14:04 +0900

    Add abc095 d

--- contest/abc/abc095/d.cpp
+++ contest/abc/abc095/d.cpp
@@ -1,51 +1,86 @@
-//
-// Created by Takaaki Hirano on 2018/09/05.
-//
+#include <cstring>
+#include <sstream>
 #include <cstdio>
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <map>
 #include <unordered_map>
 #include <cmath>
+#include <queue>
+#include <set>
+#include <numeric>
+#include <stack>
 
 using namespace std;
 typedef long long ll;
-#define INF int(1e9)
-#define REP(i, n) for(int i = 0; i < (int)(n); i++)
-#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
-#define REPONE(i, n) for(int i = 1; i <= (int)(n); i++)
-#define EACH(i, mp) for(auto i:mp)
-#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+typedef unordered_map<ll, ll> mpll;
+typedef unordered_map<char, ll> mpcl;
+typedef unordered_map<string, ll> mpsl;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using PQ = priority_queue<T>;
+template<typename T> using minPQ = priority_queue<T, vector<T>, greater<T>>;
 
+ll inl() {
+    ll x;
+    cin >> x;
+    return (x);
+}
 
-const int MAX_N = (int) 1e5;
-ll N, C, x[MAX_N], v[MAX_N], dp[MAX_N + 1], dpr[MAX_N + 1];
-
-void solve() {
-    ll ans = 0;
-    REP(i, N) {
-        REP(j, N - i) {
-            ll oa = dp[i + 1] - x[i];
-            ll ob = dpr[j + 1] - (C - x[N - j]);
-            ll ab = dp[i + 1] + dpr[j + 1] - 2 * x[i] - (C - x[N - j]);
-            ll ba = dp[i + 1] + dpr[j + 1] - 2 * (C - x[N - j]) - x[i];
-            ans = max({ans, oa, ob, ab, ba});
-        }
-    }
-    cout << ans << endl;
+string ins() {
+    string x;
+    cin >> x;
+    return (x);
 }
 
 int main() {
-    cin >> N >> C;
-    REP(i, N) {
-        cin >> x[i] >> v[i];
-        dp[i + 1] = dp[i] + v[i];
-    }
-    REP(i, N) {
-        dpr[i + 1] = dpr[i] + v[N - i];
-    }
-    solve();
-    return 0;
-}
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+#pragma clang diagnostic pop
+#endif
+        ll n = inl(), c = inl();
+        vl x(n), v(n);
+        rep(i, n) x[i] = inl(), v[i] = inl();
+        ll dl[n + 1], dr[n + 1];
+        dl[0] = 0, dr[0] = 0;
+        rep(i, n) dl[i + 1] = dl[i] + v[i];
+        rep(i, n) dr[i + 1] = dr[i] + v[n - 1 - i];
 
+        ll dp1[n + 1], dp2[n + 1];
+        dp1[0] = 0, dp2[0] = 0;
+
+        rep(i, n) {
+            dp1[i + 1] = max(dp1[i], dl[i] + v[i] - x[i]);
+            dp2[i + 1] = max(dp2[i], dr[i] + v[n - 1 - i] - (c - x[n - i - 1]));
+        }
+        ll ans = 0;
+        rep(i, n + 1) {
+            ans = max(ans, dp1[i] + dp2[n - i] - (i == 0 ? 0 : x[i - 1]));
+            ans = max(ans, dp1[i] + dp2[n - i] - (i == n ? 0 : (c - x[i])));
+        }
+        cout << ans << endl;
+
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+};
