6507d749b0aab11a5d64c13f1bac0ac01751bc91
hiramekun <thescript1210@gmail.com>
2019-04-27 14:47:23 +0900

    Add codeforces 555_div3

---
+++ contest/codeforces/555_div3/a.cpp
@@ -0,0 +1,86 @@
+#include <cstring>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <numeric>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll half_inf = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+typedef unordered_map<ll, ll> mpll;
+typedef unordered_map<char, ll> mpcl;
+typedef unordered_map<string, ll> mpsl;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using PQ = priority_queue<T>;
+template<typename T> using minPQ = priority_queue<T, vector<T>, greater<T>>;
+
+ll inl() {
+    ll x;
+    cin >> x;
+    return (x);
+}
+
+string ins() {
+    string x;
+    cin >> x;
+    return (x);
+}
+
+// if n = 123
+// ret[0] = 3, ret[1] = 2, ret[2] = 1;
+vector<int> each_digit(ll n) {
+    vector<int> ret;
+    while (n > 0) ret.emplace_back(n % 10), n /= 10;
+    return ret;
+}
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+#pragma clang diagnostic pop
+#endif
+        ll n = inl();
+        ll raw = n;
+        ll ans = 9;
+        while (n > 9) {
+            ans++;
+            while ((n + 1) % 10 != 0) {
+                n++;
+                ans++;
+            }
+            n++;
+            while (n % 10 == 0) n /= 10;
+        }
+        cout << ans << endl;
+
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
