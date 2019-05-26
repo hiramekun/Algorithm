90759abbf707bc4d220994bd439c387b5e84cc89
hiramekun <thescript1210@gmail.com>
2019-05-26 19:22:19 +0900

    Add NextPermutation

--- practice/LeetCode/ETC/NextPermutation.cpp
+++ practice/LeetCode/ETC/NextPermutation.cpp
@@ -2,22 +2,19 @@ class Solution  {
 	public:
 		void nextPermutation(vector<int>&  nums) {
 			int n = nums.size();
-			bool swapped = false;
-			for (int i = n - 1; i >= 0; i--) {
-				int cur = (int)1e9;
-				int swap_pos = i;
-				for (int j = i + 1; j < n; j++) {
-					if (nums[j] > nums[i] && nums[j] <= cur) {
-						cur = nums[j];
-						swap_pos = j;
-						swapped = true;
-					}
-				}
-				if (swapped) {
-					swap(nums[i], nums[swap_pos]);
-					break;
-				}
+
+			int idx = n - 1;
+			while (idx > 0  && nums[idx] <= nums[idx - 1]) {
+				idx--;
 			}
-			if (!swapped) sort(nums.begin(), nums.end());
+
+			if (idx > 0) {
+				int left = idx - 1;
+
+				int right = left;
+				while (right + 1 < n && nums[right + 1] > nums[left]) right++;
+				swap(nums[left], nums[right]);
+			}
+			reverse(nums.begin() + idx, nums.end());
 		}
 };
