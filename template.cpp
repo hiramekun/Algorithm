ce7fb2aab398e8f3c342b701c82e7794ce26511a
hiramekun <thescript1210@gmail.com>
2019-06-12 00:37:52 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using V = vector<T>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
 #define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
@@ -10,17 +24,6 @@ typedef long long ll;
 #define F first
 #define S second
 #define all(obj) (obj).begin(), (obj).end()
-const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-const ll mod = 1000000007;
-const ll inf = ll(1e9);
-const ll half_inf = ll(1e5);
-const ll ll_inf = ll(1e9) * ll(1e9);
-typedef unordered_map<ll, ll> mpll;
-typedef pair<ll, ll> P;
-typedef vector<ll> vl;
-typedef vector<vl> vvl;
-template<typename T> using PQ = priority_queue<T>;
-template<typename T> using minPQ = priority_queue<T, vector<T>, greater<T>>;
 
 ll inl() {
     ll x;
@@ -33,6 +36,9 @@ string ins() {
     cin >> x;
     return (x);
 }
+
+/* ----------- ANSWER ----------- */
+/* ------------------------------ */
 
 int main() {
 #ifdef MY_DEBUG
