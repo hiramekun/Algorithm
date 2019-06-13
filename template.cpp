0c51a89623505a58b64d8bd083f1e62d8e5280ac
hiramekun <thescript1210@gmail.com>
2019-06-14 04:51:01 +0900

    Update template

--- template.cpp
+++ template.cpp
@@ -10,7 +10,7 @@ template<typename T> using V = vector<T>;
 template<typename T> using pq = priority_queue<T>;
 template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
 template<typename T, typename K> using ump = unordered_map<T, K>;
-const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 const ll mod = 1000000007;
 const ll inf = ll(1e9);
 const ll e5 = ll(1e5);
@@ -19,26 +19,26 @@ const ll ll_inf = ll(1e9) * ll(1e9);
 #define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
 #define repr(i, n) for(ll i = n - 1; i >= 0; i--)
 #define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
-#define each(i, mp) for(auto i:mp)
+#define each(i, mp) for(auto& i:mp)
 #define eb emplace_back
 #define F first
 #define S second
 #define all(obj) (obj).begin(), (obj).end()
 
-ll inl() {
+inline ll inl() {
     ll x;
     cin >> x;
-    return (x);
+    return x;
 }
 
-string ins() {
+inline string ins() {
     string x;
     cin >> x;
-    return (x);
+    return x;
 }
 
-/* ----------- ANSWER ----------- */
-/* ------------------------------ */
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
 
 int main() {
 #ifdef MY_DEBUG
