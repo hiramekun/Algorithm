1ef395dfd1677b2dbfe1014aa3eeef631d446264
hiramekun <thescript1210@gmail.com>
2019-06-16 20:36:09 +0900

    Add find_sum

---
+++ util/find_sum.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using v = vector<T>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+ll inl() {
+    ll x;
+    cin >> x;
+    return (x);
+}
+
+string ins() {
+    string x;
+    cin >> x;
+    return (x);
+}
+
+// Function for finding sum of larger numbers
+string findSum(string str1, string str2) {
+    // Before proceeding further, make sure length
+    // of str2 is larger.
+    if (str1.length() > str2.length())
+        swap(str1, str2);
+
+    // Take an empty string for storing result
+    string str = "";
+
+    // Calculate length of both string
+    int n1 = str1.length(), n2 = str2.length();
+
+    // Reverse both of strings
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+
+    int carry = 0;
+    for (int i = 0; i < n1; i++) {
+        // Do school mathematics, compute sum of
+        // current digits and carry
+        int sum = ((str1[i] - '0') + (str2[i] - '0') + carry);
+        str.push_back(sum % 10 + '0');
+
+        // Calculate carry for next step
+        carry = sum / 10;
+    }
+
+    // Add remaining digits of larger number
+    for (int i = n1; i < n2; i++) {
+        int sum = ((str2[i] - '0') + carry);
+        str.push_back(sum % 10 + '0');
+        carry = sum / 10;
+    }
+
+    // Add remaining carry
+    if (carry)
+        str.push_back(carry + '0');
+
+    // reverse resultant string
+    reverse(str.begin(), str.end());
+
+    return str;
+}
+/* ----------- ANSWER ----------- */
+/* ------------------------------ */
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+#pragma clang diagnostic pop
+#endif
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
