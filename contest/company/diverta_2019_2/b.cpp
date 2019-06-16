461ed56fbc94de23ef98bcc0b8bdb591e1fc621b
hiramekun <thescript1210@gmail.com>
2019-06-16 18:24:35 +0900

    Add diverta_2019_2

---
+++ contest/company/diverta_2019_2/b.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using V = vector<T>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+ll inl() {
+    ll x;
+    cin >> x;
+    return (x);
+}
+
+string ins() {
+    string x;
+    cin >> x;
+    return (x);
+}
+
+/* ----------- ANSWER ----------- */
+/* ------------------------------ */
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+#pragma clang diagnostic pop
+#endif
+        ll n = inl();
+        V<P> xy(n);
+        rep(i, n) xy[i].F = inl(), xy[i].S = inl();
+        sort(all(xy));
+
+
+        ll ans = n;
+        rep(i, n - 1) {
+            ll tmp = n;
+            for (ll j = i + 1; j < n; ++j) {
+                ll p = xy[j].F - xy[i].F, q = xy[j].S - xy[i].S;
+
+                ll pass = 0;
+                for (ll k = 0; k < n - 1; ++k) {
+                    for (ll l = k + 1; l < n; ++l) {
+                        if (xy[l].F - xy[k].F == p && xy[l].S - xy[k].S == q) pass++;
+                    }
+                }
+                tmp = min(tmp, n - pass);
+            }
+            ans = min(tmp, ans);
+        }
+        cout << ans << endl;
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
