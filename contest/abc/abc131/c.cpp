c91accd57635bbb3dcab7ab085dee7fdefae5b44
hiramekun <thescript1210@gmail.com>
2019-06-23 01:38:23 +0900

    Add abc131

---
+++ contest/abc/abc131/c.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using v = vector<T>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+void dump_debug(T list) {
+#ifdef MY_DEBUG
+    each(e, list) cout << e << " ";
+    printf("\n");
+#endif
+}
+
+inline ll inl() {
+    ll x;
+    cin >> x;
+    return x;
+}
+
+inline string ins() {
+    string x;
+    cin >> x;
+    return x;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll m, ll n) {
+    if ((0 == m) || (0 == n)) return 0;
+    return ((m / gcd(m, n)) * n);
+}
+
+int main() {
+#ifdef MY_DEBUG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+#pragma clang diagnostic pop
+#endif
+        ll a, b, c, d;
+        cin >> a >> b >> c >> d;
+        ll div = lcm(c, d);
+        cout << b - a + 1 -
+                ((b / c + b / d) - (a - 1) / c - (a - 1) / d - b / div + (a - 1) / div)
+             << endl;
+
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
