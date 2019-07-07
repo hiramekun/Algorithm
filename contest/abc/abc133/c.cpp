86f4dabff2c145f5cf22d72bf491c43ba208cec1
hiramekun <thescript1210@gmail.com>
2019-07-08 00:25:16 +0900

    Add abc133

---
+++ contest/abc/abc133/c.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using v = vector<T>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 2019;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+void dump_debug(T list) {
+#ifdef MY_DEBUG
+    each(e, list) cout << e << " ";
+    printf("\n");
+#endif
+}
+
+inline ll inl() {
+    ll x;
+    cin >> x;
+    return x;
+}
+
+inline string ins() {
+    string x;
+    cin >> x;
+    return x;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        ll l, r;
+        cin >> l >> r;
+        ll ans = ll_inf;
+        if (r - l >= mod) cout << 0 << endl;
+        else {
+            l %= mod, r %= mod;
+            if (l > r) {
+                cout << 0 << endl;
+                return 0;
+            }
+            for (long long i = l; i <= r - 1; ++i) {
+                for (long long j = i + 1; j <= r; ++j) {
+                    ans = min(ans, (i * j) % mod);
+                }
+            }
+            cout << ans << endl;
+        }
+//        ll ans = ll_inf;
+//        if (l <= mod && mod <= r) {
+//            cout << 0 << endl;
+//        } else if (l <= mod && r <= mod) {
+//            for (long long i = 0; i < min(r - l, mod); ++i) {
+//                ll now = r - i;
+//                ans = min(ans, ((now % mod) * (now - 1) % mod) % mod);
+//            }
+//            cout << ans << endl;
+//        } else {
+//            for (long long i = 0; i < min(r - l, mod); ++i) {
+//                ll now = l + i;
+//                ans = min(ans, ((now % mod) * (now + 1) % mod) % mod);
+//            }
+//            cout << ans << endl;
+//        }
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
