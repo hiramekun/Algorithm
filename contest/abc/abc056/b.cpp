3df6d78fe0059c15e6bee8023c7b9826e7bf17e9
hiramekun <thescript1210@gmail.com>
2019-08-19 14:23:23 +0900

    Add abc056

---
+++ contest/abc/abc056/b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = n - 1; i >= 0; i--)
+#define repone(i, n) for(ll i = 1; i <= (ll)(n); i++)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+void dump_debug(T list) {
+#ifdef MY_DEBUG
+    each(e, list) cout << e << " ";
+    printf("\n");
+#endif
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        ll w, a, b;
+        cin >> w >> a >> b;
+
+        if (a > b) swap(a, b);
+        cout << max(0LL, b - (a + w)) << endl;
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
