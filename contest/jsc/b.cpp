5bfe76c916ea02532d1079658258a8cda47c067f
hiramekun <thescript1210@gmail.com>
2019-08-24 23:53:51 +0900

    Add jsc

---
+++ contest/jsc/b.cpp
@@ -0,0 +1,154 @@
+/**
+ * Created by hiramekun at 20:52 on 2019-08-24.
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+template<std::uint_fast64_t Modulus>
+class modint {
+    using u64 = std::uint_fast64_t;
+
+public:
+    u64 x;
+
+    constexpr modint(const u64 x = 0) noexcept : x(x % Modulus) {}
+
+    constexpr u64 &value() noexcept { return x; }
+
+    constexpr const u64 &value() const noexcept { return x; }
+
+    constexpr modint operator+(const modint rhs) const noexcept {
+        return modint(*this) += rhs;
+    }
+
+    constexpr modint operator-(const modint rhs) const noexcept {
+        return modint(*this) -= rhs;
+    }
+
+    constexpr modint operator*(const modint rhs) const noexcept {
+        return modint(*this) *= rhs;
+    }
+
+    constexpr modint operator/(const modint rhs) const noexcept {
+        return modint(*this) /= rhs;
+    }
+
+    constexpr modint &operator+=(const modint rhs) noexcept {
+        x += rhs.x;
+        if (x >= Modulus) {
+            x -= Modulus;
+        }
+        return *this;
+    }
+
+    constexpr modint &operator-=(const modint rhs) noexcept {
+        if (x < rhs.x) {
+            x += Modulus;
+        }
+        x -= rhs.x;
+        return *this;
+    }
+
+    constexpr modint &operator*=(const modint rhs) noexcept {
+        x = x * rhs.x % Modulus;
+        return *this;
+    }
+
+    constexpr modint &operator/=(modint rhs) noexcept {
+        u64 exp = Modulus - 2;
+        while (exp) {
+            if (exp % 2) {
+                *this *= rhs;
+            }
+            rhs *= rhs;
+            exp /= 2;
+        }
+        return *this;
+    }
+};
+
+typedef vector<modint<mod>> vmod;
+typedef vector<vmod> vvmod;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vl a(n);
+    rep(i, n) cin >> a[i];
+
+    vl inis(n);
+    rep(i, n - 1) {
+        for (ll j = i + 1; j < n; ++j) {
+            if (a[i] > a[j]) inis[i]++;
+        }
+    }
+    vl vals(n);
+    rep(i, n) {
+        rep(j, n) {
+            if (a[i] > a[j]) vals[i]++;
+        }
+    }
+    modint<mod> ans = 0;
+    rep(i, n) {
+        modint<mod> tmp = 0;
+        tmp += 2 * inis[i];
+        tmp += (k - 1) * vals[i];
+        tmp *= k;
+        tmp /= 2;
+        ans += tmp;
+    }
+
+
+    cout << ans.x << '\n';
+
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
