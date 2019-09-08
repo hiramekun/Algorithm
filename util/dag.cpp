ff4262550095ee9635e0f724c48d815ce7abecba
hiramekun <thescript1210@gmail.com>
2019-09-08 16:11:05 +0900

    Update dag

--- util/dag.cpp
+++ util/dag.cpp
@@ -21,18 +21,16 @@ typedef vector<ve> vve;
 
 class DAG {
 private:
-    ll v, e;
+    ll v;
     vve table;
     vl d;
     vl pre; // 最短路の直前の頂点
     ve edges;
 public:
     // v: 頂点数
-    // e: 辺の数
-    DAG(ll v, ll e) : v(v), e(e) {
+    explicit DAG(ll v) : v(v) {
         table.resize(v);
         pre.resize(v, -1);
-        edges.resize(e);
         d.resize(v, ll_inf);
     }
 
@@ -42,13 +40,72 @@ public:
         edges.emplace_back(e1);
     }
 
+    // O(v + e)
+    // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B
+    vl sort() {
+        stack<ll> st; // 入力次数が0の頂点集合
+        vl in_d(v);
+        for (const auto ele: edges) {
+            in_d[ele.to]++;
+        }
+
+        rep(i, v) if (in_d[i] == 0) {
+                st.push(i);
+            }
+        vl ans; // ソート後のグラフ
+
+        while (!st.empty()) {
+            ll i = st.top();
+            st.pop();
+            ans.emplace_back(i);
+            for (const auto j: table[i]) {
+                ll to = j.to;
+                in_d[to]--;
+                if (in_d[to] == 0) st.push(to);
+            }
+        }
+
+        return move(ans);
+    }
+
+    // O(v + e)
+    // 閉路が存在するときは-1を返す
+    ll get_longest_path() {
+        vector<bool> seen(v), calc(v);
+        vl d(v, 0);
+        auto dfs = [&](auto &&f, ll now) -> ll {
+            if (seen[now]) {
+                if (!calc[now]) return -1;
+                return d[now];
+            }
+            seen[now] = true;
+            d[now] = 1;
+            for (const auto &t: table[now]) {
+                ll res = f(f, t.to);
+                if (res == -1) return -1;
+                d[now] = max(res + t.cost, d[now]);
+            }
+            calc[now] = true;
+            return d[now];
+        };
+
+        ll ans = 0;
+        rep(i, v) {
+            ll tmp = dfs(dfs, i);
+            if (tmp == -1) {
+                return -1;
+            }
+            ans = max(tmp, ans);
+        }
+        return ans;
+    }
+
     // 負の重みがあっても動く
     // O(v * e)
     vl bellman_ford(ll s) {
         d[s] = 0;
-        for (int j = 0; j < v - 1; j++) {
-            for (int i = 0; i < e; ++i) {
-                edge e1 = edges[i];
+        rep(_, v - 1) {
+            for (const auto &e1 : edges) {
                 if (d[e1.from] != ll_inf && d[e1.to] > d[e1.from] + e1.cost) {
                     d[e1.to] = d[e1.from] + e1.cost;
                 }
@@ -61,10 +118,8 @@ public:
     // bellman_fortで最短経路を出した後に使う
     vl has_negative_loop_in_path() {
         vl has_loop(v, false);
-        for (ll i = 0; i < v - 1; ++i) {
-            for (ll j = 0; j < e; ++j) {
-                edge e1 = edges[i];
-
+        rep(_, v - 1) {
+            for (const auto &e1 : edges) {
                 if (d[e1.to] > d[e1.from] + e1.cost) {
                     has_loop[e1.from] = true;
                 }
