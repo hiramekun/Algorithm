1ac9f6d419f2b88a80a4dd98f7e963a1fb0d8e64
hiramekun <thescript1210@gmail.com>
2019-10-29 23:03:34 +0900

    Add abc033 c

---
+++ contest/abc/abc033/c.cpp
@@ -0,0 +1,92 @@
+/**
+ * Created by hiramekun at 17:12 on 2019-10-28.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+vector<string> split(const string &str, char sep) {
+    vector<string> v;
+    stringstream ss(str);
+    string buffer;
+    while (getline(ss, buffer, sep)) {
+        v.push_back(buffer);
+    }
+    return v;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    vector<string> res = split(s, '+');
+    ll ans = 0;
+    each(e, res) {
+        ll n = e.size();
+        bool has0 = false;
+        rep(i, n) {
+            if (e[i] == '*') {
+                continue;
+            } else {
+                has0 = has0 || e[i] == '0';
+            }
+        }
+        if (!has0) ans++;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
