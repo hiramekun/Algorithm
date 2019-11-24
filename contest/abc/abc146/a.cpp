5c5c4c6ff45dd08ea3899627d2920964f6d28bc1
hiramekun <thescript1210@gmail.com>
2019-11-25 00:41:27 +0900

    Add abc146

---
+++ contest/abc/abc146/a.cpp
@@ -0,0 +1,77 @@
+/**
+ * Created by hiramekun at 20:47 on 2019-11-24.
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using P = pair<ll, ll>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T, typename K> using ump = unordered_map<T, K>;
+const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const ll mod = 1000000007;
+const ll inf = ll(1e9);
+const ll e5 = ll(1e5);
+const ll ll_inf = ll(1e9) * ll(1e9);
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define repr(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+#define each(i, mp) for(auto& i:mp)
+#define eb emplace_back
+#define F first
+#define S second
+#define all(obj) (obj).begin(), (obj).end()
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << ' ';
+    return out;
+}
+
+template<class T>
+istream &operator>>(istream &in, vector<T> &list) {
+    ll n = list.size();
+    rep(i, n) in >> list[i];
+    return in;
+}
+
+template<class T>
+ostream &operator<<(ostream &out, const vector<vector<T>> &list) {
+    ll n = list.size();
+    rep(i, n) out << list[i] << '\n';
+    return out;
+}
+
+/* ------------- ANSWER ------------- */
+/* ---------------------------------- */
+
+void solve() {
+    string s;
+    cin >> s;
+    ll ans = 0;
+    if (s == "SUN") ans = 7;
+    else if (s == "MON") ans = 6;
+    else if (s == "TUE") ans = 5;
+    else if (s == "WED") ans = 4;
+    else if (s == "THU") ans = 3;
+    else if (s == "FRI") ans = 2;
+    else if (s == "SAT") ans = 1;
+    cout << ans << '\n';
+}
+
+int main() {
+#ifdef MY_DEBUG
+    while (true) {
+#endif
+        solve();
+#ifdef MY_DEBUG
+    }
+#endif
+    return 0;
+}
