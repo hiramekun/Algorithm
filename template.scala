4c6e9eec217e79264077e935fef025b9e214933b
hiramekun <thescript1210@gmail.com>
2020-11-01 20:43:39 +0900

    Add scala template

---
+++ template.scala
@@ -0,0 +1,20 @@
+import Util._
+import scala.io.StdIn._
+
+object Main extends App {
+}
+
+object Util {
+  def rep(n: Int)(f: Int => Unit): Unit = for (i <- 0 until n) f(i)
+  def repYield[T](n: Int)(f: Int => T): IndexedSeq[T] = for (i <- 0 until n) yield f(i)
+
+  implicit class Extend[T](value: T) {
+    def also(func: T => Unit): T = {
+      func(value)
+      value
+    }
+
+    def let[A](func: T => A): A = func(value)
+  }
+
+}
