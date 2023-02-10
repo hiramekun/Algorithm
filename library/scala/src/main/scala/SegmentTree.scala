5387854feb0fc83b963540229427f162e863a74a
hiramekun <thescript1210@gmail.com>
2023-02-11 00:46:00 +0900

    use monoid trait

--- library/scala/src/main/scala/SegmentTree.scala
+++ library/scala/src/main/scala/SegmentTree.scala
@@ -1,116 +1,132 @@
 import scala.io.StdIn.readLine
+import scala.reflect.ClassTag
 
-object SegmentTree {
-  type Data = Long
-  type Lazy = Long
-  type MergeData = (Data, Data) => Data
-  type UpdateLazyFromX = (Lazy, Lazy) => Lazy
-  type UpdateDataFromLazy = (Data, Lazy) => Data
-  type CalcLazyWithLen = (Lazy, Int) => Lazy
+trait Monoid[T] {
+  val combineFn: (T, T) => T
 
-  def rangeMinimumQuery(n: Int): SegmentTree =
-    new SegmentTree(
-      n = n,
-      fm = (a, b) => a min b,
-      fl = (_, b) => b,
-      fa = (_, b) => b,
-      fw = (a, _) => a,
-      M1 = Int.MaxValue,
-      A1 = Int.MaxValue
-    )
-
-  def rangeMaxQuery(n: Int): SegmentTree =
-    new SegmentTree(
-      n = n,
-      fm = (a, b) => a max b,
-      fl = (_, b) => b,
-      fa = (_, b) => b,
-      fw = (a, _) => a,
-      M1 = Int.MinValue,
-      A1 = Int.MinValue
-    )
-
-  def rangeSumTree(n: Int): SegmentTree =
-    new SegmentTree(
-      n = n,
-      fm = (a, b) => a + b,
-      fl = (a, b) => a + b,
-      fa = (a, b) => a + b,
-      fw = (a, w) => a * w,
-      M1 = 0,
-      A1 = 0
-    )
+  def zero: T
 }
 
-import SegmentTree._
+trait LazyOperation[T] {
+  def updateLazyFromData(l: T, data: T): T
 
-class SegmentTree
-(val n: Int,
- val fm: MergeData,
- val fl: UpdateLazyFromX,
- val fa: UpdateDataFromLazy,
- val fw: CalcLazyWithLen,
- val M1: Data,
- val A1: Lazy) {
+  def updateDataFromLazy(data: T, l: T): T
+
+  def calcLazyWithLen(l: T, len: Int): T
+}
+
+trait LazyMonoid[T] extends Monoid[T] with LazyOperation[T]
+
+class SegmentTree[T](val n: Int)(implicit monoid: LazyMonoid[T], classTag: ClassTag[T]) {
+
+  import monoid._
+
   var sz = 1
   while (sz < n) sz *= 2
-  val seg = Array.fill[Data](2 * sz - 1)(M1)
-  val _lazy = Array.fill[Lazy](2 * sz - 1)(A1)
+  val seg = Array.fill[T](2 * sz - 1)(zero)
+  val _lazy = Array.fill[T](2 * sz - 1)(zero)
 
-  def update(i: Int, x: Data): Unit = {
+  def update(i: Int, x: T): Unit = {
     var i2 = i + sz - 1
     seg(i2) = x
     while (i2 > 0) {
       i2 = (i2 - 1) / 2
-      seg(i2) = fm(seg(2 * i2 + 1), seg(2 * i2 + 2))
+      seg(i2) = combineFn(seg(2 * i2 + 1), seg(2 * i2 + 2))
     }
   }
 
-  def build(v: Array[Data]): Unit = {
+  def build(v: Array[T]): Unit = {
     for (i <- 0 until n) seg(i + sz - 1) = v(i)
     for (i <- sz - 2 to 0 by -1) {
-      seg(i) = fm(seg(2 * i + 1), seg(2 * i + 2))
+      seg(i) = combineFn(seg(2 * i + 1), seg(2 * i + 2))
     }
   }
 
-  def update(a: Int, b: Int, x: Data): Unit = update(a, b, x, 0, 0, sz)
+  def update(a: Int, b: Int, x: T): Unit = update(a, b, x, 0, 0, sz)
 
-  def query(a: Int, b: Int): Data = query(a, b, 0, 0, sz)
+  def query(a: Int, b: Int): T = query(a, b, 0, 0, sz)
 
   private def eval(len: Int, k: Int): Unit = {
-    if (_lazy(k) == A1) return
+    if (_lazy(k) == zero) return
     if (2 * k + 1 < sz * 2 - 1) {
-      _lazy(2 * k + 1) = fl(_lazy(2 * k + 1), _lazy(k))
-      _lazy(2 * k + 2) = fl(_lazy(2 * k + 2), _lazy(k))
+      _lazy(2 * k + 1) = updateLazyFromData(_lazy(2 * k + 1), _lazy(k))
+      _lazy(2 * k + 2) = updateLazyFromData(_lazy(2 * k + 2), _lazy(k))
     }
-    seg(k) = fa(seg(k), fw(_lazy(k), len))
-    _lazy(k) = A1
+    seg(k) = updateDataFromLazy(seg(k), calcLazyWithLen(_lazy(k), len))
+    _lazy(k) = zero
   }
 
-  private def update(a: Int, b: Int, x: Data, k: Int, l: Int, r: Int): Unit = {
+  private def update(a: Int, b: Int, x: T, k: Int, l: Int, r: Int): Unit = {
     eval(r - l, k)
     if (r <= a || b <= l) return
     if (a <= l && r <= b) {
-      _lazy(k) = fl(_lazy(k), x)
+      _lazy(k) = updateLazyFromData(_lazy(k), x)
       eval(r - l, k)
     } else {
       val m = (l + r) / 2
       update(a, b, x, 2 * k + 1, l, m)
       update(a, b, x, 2 * k + 2, m, r)
-      seg(k) = fm(seg(2 * k + 1), seg(2 * k + 2))
+      seg(k) = combineFn(seg(2 * k + 1), seg(2 * k + 2))
     }
   }
 
-  private def query(a: Int, b: Int, k: Int, l: Int, r: Int): Data = {
-    if (r <= a || b <= l) return M1
+  private def query(a: Int, b: Int, k: Int, l: Int, r: Int): T = {
+    if (r <= a || b <= l) return zero
     eval(r - l, k)
     if (a <= l && r <= b) return seg(k)
     val m = (l + r) / 2
-    fm(query(a, b, 2 * k + 1, l, m), query(a, b, 2 * k + 2, m, r))
+    combineFn(query(a, b, 2 * k + 1, l, m), query(a, b, 2 * k + 2, m, r))
   }
 }
 
-// verified with https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=7456366#2
+object SegmentTree {
+  def rangeMinimumQuery(n: Int): SegmentTree[Long] = {
+    implicit object LongMinMonoid extends LazyMonoid[Long] {
+      override val combineFn: (Long, Long) => Long = _ min _
+
+      override def zero: Long = Int.MaxValue
+
+      override def updateLazyFromData(l: Long, data: Long): Long = data
+
+      override def updateDataFromLazy(data: Long, l: Long): Long = l
+
+      override def calcLazyWithLen(l: Long, len: Int): Long = l
+    }
+    new SegmentTree[Long](n)
+  }
+
+  def rangeMaxQuery(n: Int): SegmentTree[Long] = {
+    implicit object LongMaxMonoid extends LazyMonoid[Long] {
+      override val combineFn: (Long, Long) => Long = _ max _
+
+      override def zero: Long = Long.MinValue
+
+      override def updateLazyFromData(l: Long, data: Long): Long = data
+
+      override def updateDataFromLazy(data: Long, l: Long): Long = l
+
+      override def calcLazyWithLen(l: Long, len: Int): Long = l
+    }
+    new SegmentTree[Long](n)
+  }
+
+  def rangeSumTree(n: Int): SegmentTree[Long] = {
+    implicit object LongSumMonoid extends LazyMonoid[Long] {
+      override val combineFn: (Long, Long) => Long = _ + _
+
+      override def zero: Long = 0L
+
+      override def updateLazyFromData(l: Long, data: Long): Long = l + data
+
+      override def updateDataFromLazy(data: Long, l: Long): Long = data + l
+
+      override def calcLazyWithLen(l: Long, len: Int): Long = l * len
+    }
+    new SegmentTree[Long](n)
+  }
+}
+
+// verified with https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=7460199#2
 object SegmentTreeVerify {
   def main(args: Array[String]): Unit = {
     val nq = readLine().split(" ").map(_.toInt)
